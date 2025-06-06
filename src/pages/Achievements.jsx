d25128bd4d78640716530a66223b5564a4c9bdef
Camille <yuri.soushi@gmail.com>
2025-06-06 18:39:35 +0800

    Added redirect

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -3,21 +3,22 @@ import styles from "../components/css/Archievments.module.css";
 import { useParams } from 'react-router-dom';
 import TotalAchievement from "../components/TotalAchievement";
 import DailyAchievement from "../components/DailyAchievement";
+import { BrowserRouter as Link} from "react-router-dom";
 const Achievements = () => {
     const { slug } = useParams();
 
 
-    if ( slug === 'composition' || slug === undefined) {
-    return (
-        <div className={`${styles.archievments} wrapper`}>
-            <ul>
-                <li><a href="/achievements/">フラッシュカード</a></li>
-                <li><a href="/achievements/composition/">瞬間英作文</a></li>
-            </ul>
-            <TotalAchievement slug={slug}/>
-            <DailyAchievement slug={slug}/>
-        </div>
-    );
+    if (slug === 'composition' || slug === undefined) {
+        return (
+            <div className={`${styles.archievments} wrapper`}>
+                <ul>
+                    <li><Link to="/achievements/">フラッシュカード</Link></li>
+                    <li><Link to="/achievements/composition/">瞬間英作文</Link></li>
+                </ul>
+                <TotalAchievement slug={slug} />
+                <DailyAchievement slug={slug} />
+            </div>
+        );
     } else {
         return <h1>404 - Page Not Found</h1>;
     }
